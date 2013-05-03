4e062ef2d42b098fe4cbfb097a3ce19fb61cbf38
Adrian Jurcevic <agerage@me.com>
2013-05-03 17:14:35 +0800

    Combined Version

--- DistressProto/AppDelegate.h
+++ DistressProto/AppDelegate.h
@@ -7,15 +7,54 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreTelephony/CTCall.h>
+#import <CoreTelephony/CTCallCenter.h>
+#import <CoreLocation/CoreLocation.h>
 
-@class MainViewController;
+@class CallingViewController;
+@class OptionViewController;
+@class MainContactViewController;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, UIAlertViewDelegate>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UIAlertViewDelegate, CLLocationManagerDelegate>
+{
+    UINavigationController *navController;
+    MainContactViewController *contactsViewController;
+    CallingViewController *callingViewController;
+    OptionViewController *optionViewController;
+    CTCallCenter *callCenter;
+    
+    NSArray *contacts;
+    NSArray *contactsNames;
+    NSArray *contactsRelation;
+    NSArray *contactsImages;
+    NSString *locationAddressString;
+    
+    CLLocationManager *locationManager;
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
+    NSString *latitude;
+    NSString *longitude;
+    
+    BOOL userHasStartedCall;
+    bool phoneHasEnteredBackground;
+    
+    
+    NSInteger currentIndex;
+    
+    NSInteger startingIndex;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) MainViewController *mainViewController;
+@property (nonatomic, readwrite) BOOL userHasStartedCall;
 
-@property (strong, nonatomic) UINavigationController *navigationController;
+@property (nonatomic, readwrite) NSInteger *testInt;
+@property (nonatomic, readonly) NSInteger currentIndex;
+
+-(void)startCallCycleAt:(NSInteger)startIndex;
+-(void)callNextPerson;
+-(void)presentOptionViewWithIndex:(NSInteger)positionIndex;
+-(void)endCallCycle;
 
 @end
